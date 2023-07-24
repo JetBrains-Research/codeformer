label provided to the loss calculation are fixed

jetnn/models/tasks/code_modelling.py
@@ -62,8 +62,8 @@ class CodeModellingModel(LightningModule):
     # double check indexes
     def _shared_step(self, batch, step):
         logits = self(batch, step)
-        logits = logits[1:-1]
-        batch.label_tokens = batch.label_tokens[1:]
+        logits = logits[1:]
+        # batch.label_tokens = batch.label_tokens[1:]
         result = {f"{step}/loss": self._loss(logits, batch.label_tokens)}
         with torch.no_grad():
             prediction = logits.argmax(-1)