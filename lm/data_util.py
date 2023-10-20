3e5a01246f1601a206f27ee227d961197c880540
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-20 19:28:21 +0200

    add: The Pile dataloader

---
+++ lm/data_util.py
@@ -0,0 +1,70 @@
+from typing import List, Optional
+
+from pytorch_lightning import LightningDataModule
+from tokenizers import Tokenizer
+import torch
+from torch.utils.data import DataLoader
+
+from jetnn.data_processing.tasks.language_modeling import BatchedTextTokens, TextTokens
+from jetnn.data_processing.tasks.task_specific_datasets.language_modeling_dataset import ThePileDataset
+
+
+class ThePileDataModule(LightningDataModule):
+    def __init__(self,
+                 batch_size: int,
+                 tokenizer: Tokenizer,
+                 max_text_tokens: int,
+                 max_chunks_number: int,
+                 max_chunk_size: int,
+                 num_workers: int = 16,
+                 prefetch_factor: int = 8) -> None:
+        super().__init__()
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        self.max_text_tokens = max_text_tokens
+        self.max_chunks_number = max_chunks_number
+        self.max_chunk_size = max_chunk_size
+        self.num_workers = num_workers
+        self.prefetch_factor = prefetch_factor
+
+    @staticmethod
+    def collate_wrapper(
+            batch: List[Optional[TextTokens]],
+    ) -> BatchedTextTokens:
+        return BatchedTextTokens(batch)
+
+    def _create_dataset(self, split: str):
+        return ThePileDataset(split,
+                              self.tokenizer,
+                              self.max_text_tokens,
+                              self.max_chunks_number,
+                              self.max_chunk_size)
+
+    def _shared_dataloader(self, split: str) -> DataLoader:
+        ds = self._create_dataset(split)
+        return DataLoader(ds,
+                          batch_size=self.batch_size,
+                          collate_fn=self.collate_wrapper,
+                          prefetch_factor=self.prefetch_factor,
+                          num_workers=self.num_workers)
+
+    def train_dataloader(self, *args, **kwargs) -> DataLoader:
+        return self._shared_dataloader('train')
+
+    def val_dataloader(self, *args, **kwargs) -> DataLoader:
+        return self._shared_dataloader('validation')
+
+    def test_dataloader(self, *args, **kwargs) -> DataLoader:
+        return self._shared_dataloader('test')
+
+    def predict_dataloader(self, *args, **kwargs) -> DataLoader:
+        raise NotImplementedError
+
+    def transfer_batch_to_device(
+            self,
+            batch: BatchedTextTokens,
+            device: torch.device,
+            dataloader_idx: int,
+    ) -> BatchedTextTokens:
+        batch.move_to_device(device)
+        return batch
