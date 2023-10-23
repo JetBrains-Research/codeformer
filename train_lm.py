b2894dfe90870dfb6552db4c2ff76d823a909f46
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-23 15:49:37 +0200

    params: learning rate and weight decay + tweak num_workers

--- train_lm.py
+++ train_lm.py
@@ -1,33 +1,48 @@
 import torch
 from transformers import AutoTokenizer
 from tqdm import tqdm
+import wandb
 
 from lm.model import CodeformerLM
 from lm.data_utils import ThePileDataModule
+from lm.utils import setup_wandb
 
 
 def main():
-    batch_size = 2
+    setup_wandb()
+    batch_size = 4
     model_name = 'microsoft/deberta-v3-small'
 
     max_text_tokens = 512
     max_chunk_size = 14
     max_chunks_number = 32
     weight_decay = 0.0
+    learning_rate = 3e-5
 
     # TODO: write parameters gathering for weight_decay
     assert weight_decay == 0.0
 
+    min_tokens = 64
+    min_chunks = 8
+
+    num_workers_dl = 16
+    prefetch_factor_dl = 32
+    # num_workers_dl = 0
+    # prefetch_factor_dl = None
+
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     dm = ThePileDataModule(batch_size, tokenizer, max_text_tokens,
-                           max_chunks_number, max_chunk_size, num_workers=0,
-                           prefetch_factor=None)
+                           max_chunks_number, max_chunk_size,
+                           min_tokens, min_chunks,
+                           num_workers=num_workers_dl,
+                           prefetch_factor=prefetch_factor_dl)
+
     dl = dm.train_dataloader()
     train_iterator = tqdm(dl)
 
     device = torch.device('cuda:0')
     model = CodeformerLM(model_name).to(device)
-    opt = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.0)
+    opt = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     for batch in train_iterator:
         opt.zero_grad()
@@ -35,6 +50,8 @@ def main():
         loss_tens = model(batch)
         loss = loss_tens.sum() / torch.count_nonzero(loss_tens)
         loss.backward()
+        wandb.log({'loss': loss})
+        train_iterator.set_description(f'Loss: {loss.item():.3f}')
         opt.step()
 
 
