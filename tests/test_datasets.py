8a0ef3c014ab27fef441c2231703babe0cb1f298
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-25 16:24:41 +0200

    refactor: dataset test to share common args

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -6,23 +6,23 @@ from lm.data_utils import (WikiText2Dataset, WikiText103Dataset,
 
 TEST_TOKENIZER_NAME = 'microsoft/deberta-v3-base'
 
+MAX_TEXT_TOKENS = 2048
+MAX_CHUNK_SIZE = 14
+MAX_CHUNKS_NUMBER = 384
+MIN_CHUNKS = 8
+MIN_TOKENS = 128
+BATCH_SIZE = 2
+
 
 def test_wikitext_datasets():
     ...
 
 
 def test_the_pile_data_module():
-    max_text_tokens = 2048
-    max_chunk_size = 14
-    max_chunks_number = 384
-    min_chunks = 8
-    min_tokens = 128
-    batch_size = 2
     tokenizer = AutoTokenizer.from_pretrained(TEST_TOKENIZER_NAME)
-    dm = ThePileDataModule(batch_size, tokenizer, max_text_tokens,
-                           max_chunks_number, max_chunk_size, min_tokens,
-                           min_chunks, num_workers=0, prefetch_factor=None)
+    dm = ThePileDataModule(BATCH_SIZE, tokenizer, MAX_TEXT_TOKENS,
+                           MAX_CHUNKS_NUMBER, MAX_CHUNK_SIZE, MIN_TOKENS,
+                           MIN_CHUNKS, num_workers=0, prefetch_factor=None)
     train_dl = dm.train_dataloader()
     batch = next(iter(train_dl))
-    a = 'pass'
     # TODO: add tensor checks
