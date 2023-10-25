9056e27d01047d5146161624bb22958120da2d0f
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-25 19:32:20 +0200

    feat: hydra configs

--- lm/utils.py
+++ lm/utils.py
@@ -4,12 +4,10 @@ import wandb
 
 
 def setup_wandb(args=None):
-    # mode = 'disabled' if args['dbg'] else None
-    # run = wandb.init(project="codeformer", mode=mode)
-    run = wandb.init(project="codeformer")
-    # wandb.run.name = args['name']
-    wandb.run.name = 'test'
-    # wandb.config.update(args)
+    mode = 'disabled' if args['dbg'] else None
+    run = wandb.init(project="codeformer", mode=mode)
+    wandb.run.name = args['name']
+    wandb.config.update(args)
     return run
 
 
--- train_lm.py
+++ train_lm.py
@@ -1,51 +1,58 @@
+import hydra
 import torch
 from transformers import AutoTokenizer
 from tqdm import tqdm
 import wandb
 
 from lm.model import CodeformerLM
-from lm.data_utils import ThePileDataModule
+from lm.data_utils import ThePileDataModule, WikiText2RawDataModule
 from lm.utils import setup_wandb
 
+@hydra.main('configs', 'wikitext2', version_base=None)
+def main(args):
+    print(args)
 
-def main():
-    setup_wandb()
-    batch_size = 64
-    micro_batch_size = 4
-    assert batch_size % micro_batch_size == 0
-    accumulation_factor = batch_size // micro_batch_size
-    model_name = 'microsoft/deberta-v3-small'
+    setup_wandb(args)
+    args.batch_size = 128
+    args.micro_batch_size = 4
+    assert args.batch_size % args.micro_batch_size == 0
+    accumulation_factor = args.batch_size // args.micro_batch_size
 
-    max_text_tokens = 512
-    max_chunk_size = 14
-    max_chunks_number = 32
-    weight_decay = 0.0
-    learning_rate = 3e-5
+    args.max_text_tokens = 512
+    args.max_chunk_size = 14
+    args.max_chunks_number = 32
+    args.weight_decay = 0.0
+    args.learning_rate = 1e-4
 
     # TODO: write parameters gathering for weight_decay
-    assert weight_decay == 0.0
+    assert args.weight_decay == 0.0
 
-    min_tokens = 64
-    min_chunks = 8
+    args.min_tokens = 64
+    args.min_chunks = 8
 
-    num_workers_dl = 16
-    prefetch_factor_dl = 32
+    args.num_workers_dl = 16
+    args.prefetch_factor_dl = 32
     # num_workers_dl = 0
     # prefetch_factor_dl = None
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    dm = ThePileDataModule(micro_batch_size, tokenizer, max_text_tokens,
-                           max_chunks_number, max_chunk_size,
-                           min_tokens, min_chunks,
-                           num_workers=num_workers_dl,
-                           prefetch_factor=prefetch_factor_dl)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    # dm = ThePileDataModule(args.micro_batch_size, tokenizer, args.max_text_tokens,
+    #                        args.max_chunks_number, args.max_chunk_size,
+    #                        args.min_tokens, args.min_chunks,
+    #                        num_workers=args.num_workers_dl,
+    #                        prefetch_factor=args.prefetch_factor_dl)
+    dm = WikiText2RawDataModule(args.micro_batch_size, tokenizer, args.max_text_tokens,
+                                args.max_chunks_number, args.max_chunk_size,
+                                args.min_tokens, args.min_chunks,
+                                num_workers=args.num_workers_dl,
+                                prefetch_factor=args.prefetch_factor_dl)
 
     dl = dm.train_dataloader()
     train_iterator = tqdm(dl)
 
     device = torch.device('cuda:0')
-    model = CodeformerLM(model_name).to(device)
-    opt = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    model = CodeformerLM(args.model_name).to(device)
+    opt = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
     processed_batches = 0
     losses_micro_batches = []
