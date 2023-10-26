4a02bcfb4bba1f30060b48acb0ca5390e4ba07c6
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-26 14:57:55 +0200

    util: dump wikitext datasets

--- lm/utils.py
+++ lm/utils.py
@@ -31,3 +31,29 @@ def perplexity(logits: Tensor, targets: LongTensor, pad_id: int) -> Tensor:
     loss = loss_tens.sum() / torch.count_nonzero(loss_tens)
     ppl = loss.exp()
     return ppl
+
+
+def dump_wikitext_dataset(dump_dir: str | Path | None = None) -> None:
+    if dump_dir is None:
+        dump_dir = Path('./')
+    max_text_tokens = 2048
+    max_chunk_size = 14
+    max_chunks_number = 384
+    min_chunks = 8
+    min_tokens = 128
+    tokenizer = None
+    ds_to_file_name = {
+        WikiText2Dataset: 'wiki-text-2',
+        WikiText2RawDataset: 'wiki-text-2-raw',
+        WikiText103Dataset: 'wiki-text-103',
+        WikiText103RawDataset: 'wiki-text-103-raw',
+    }
+    for dataset_class in WIKITEXT_DATASET_CLASSES:
+        for split in ['train', 'validation', 'test']:
+            ds = dataset_class(split, tokenizer, max_text_tokens,
+                               max_chunks_number, max_chunk_size,
+                               min_chunks, min_tokens)
+            with open(dump_dir / f'{ds_to_file_name[dataset_class]}-{split}.jsonl', 'w') as fp:
+                for sample in ds.ds:
+                    fp.write(json.dumps({'text': sample}) + '\n')
+
