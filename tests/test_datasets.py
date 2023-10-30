e77fc17c744feb8b8032d0620a7dd027d0f4cdce
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-10-30 12:06:18 +0100

    test: limit max samples in the dataset tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -16,13 +16,18 @@ from lm.utils import WIKITEXT_DATASET_CLASSES
 
 @pytest.mark.parametrize('ds_class,split', product(WIKITEXT_DATASET_CLASSES, WIKITEXT_SPLITS))
 def test_wikitext_datasets(ds_class: WikiTextDatasetBase, split: str):
+    max_samples_to_test = 1000
     tokenizer = AutoTokenizer.from_pretrained(TEST_TOKENIZER_NAME)
     ds = ds_class(split, tokenizer, MAX_TEXT_TOKENS, MAX_CHUNKS_NUMBER, MAX_CHUNK_SIZE, MIN_CHUNKS, MIN_TOKENS)
+    count = 0
     for sample in ds:
         assert torch.tensor(sample.token_ids, dtype=torch.long).ndim == 1
         assert torch.tensor(sample.split_sizes, dtype=torch.long).ndim == 1
         assert len(sample.token_ids) > 0
         assert len(sample.split_sizes) > 0
+        count += 1
+        if count > max_samples_to_test:
+            break
 
 
 @pytest.mark.parametrize('dataset_name', AllDatasetsDataModule.name_to_dataset.keys())
