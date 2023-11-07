431b10e09ac4af2d2e91fc27564e2e8da09f01f3
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-07 19:54:12 +0100

    feat: add disassemble tensor util

--- lm/utils.py
+++ lm/utils.py
@@ -3,9 +3,8 @@ import json
 from pathlib import Path
 
 import torch
-from torch import nn
+from torch import nn, Tensor, LongTensor
 from omegaconf import OmegaConf
-from torch import Tensor
 import wandb
 from tokenizers import Tokenizer
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, PreTrainedModel
@@ -156,3 +155,83 @@ def check_tokenizer_pad_id(tokenizer: Tokenizer) -> None:
     assert (tokenizer.eos_token_id is not None) and (tokenizer.eos_token_id != tokenizer.pad_token_id)
     # assert (tokenizer.sos_token_id != tokenizer.bos_token_id)  # it seems to be ok
     assert tokenizer.pad_token_id is not None
+
+
+def expand_filler(fill_empty_val: int | float | Tensor, inputs_shape: torch.Size) -> Tensor:
+    is_scalar_tensor = isinstance(fill_empty_val, Tensor) and fill_empty_val.numel() == 1
+    # Expand filler if needed
+    if isinstance(fill_empty_val, int) or is_scalar_tensor:
+        filler = torch.full([inputs_shape[0]] + [1] + list(inputs_shape)[2:],
+                            fill_empty_val)
+        # print(filler.shape)
+    else:
+        filler = fill_empty_val.view(1, 1, *fill_empty_val.shape).repeat(inputs_shape[0], 1, *([1] * len(fill_empty_val.shape)))
+    return filler
+
+
+def disassemble(inputs: Tensor, sizes: LongTensor, fill_empty_val: Tensor | int | float) -> Tensor:
+    # inputs.shape = [batch_size, seq_len, *other_shapes]
+    # sizes.shape = [batch_size, max_chunks_per_sample]
+    # fill_empty_val.shape = [*other_shapes] where other shapes can be empty
+    #   so that the inputs are 2D, OR scalar
+
+    # Example (intermediate values for this example will be shown further with "E:" signature):
+    #
+    # inputs:
+    # [[1, 2, 3],
+    #  [4, 5, 6]]
+    #
+    # sizes:
+    # [[2, 1],
+    #  [3, 0]]
+    #
+    # fill_empty_val:
+    # 0
+    #
+    # Expected output:
+    # [[1, 2, 0],
+    #  [3, 0, 0],
+    #  [4, 5, 6]]
+
+    device = inputs.device
+    batch_size = inputs.shape[0]
+    num_chunks_total = sizes.count_nonzero()  # E: 3
+    max_chunk_length = sizes.max()  # E: 3
+    num_chunks_per_sample = sizes.count_nonzero(dim=0)  # E: [2, 1]
+
+    num_chunks_range = torch.arange(1, num_chunks_total + 1, device=device).view(num_chunks_total, 1)  # E: [[1], [2], [3]]
+    num_chunkscum_sum = num_chunks_per_sample.cumsum(0).view(1, batch_size)  # E: [[2, 3]]
+    # E: num_chunks_range > num_chunkscum_sum =
+    # [[0, 0]
+    #  [0, 0],
+    #  [1, 0]]
+    sample_nums = torch.sum(num_chunks_range > num_chunkscum_sum, 1)  # E: [0, 0, 1]
+
+    sample_nums = sample_nums.view(num_chunks_total, 1).repeat(1, max_chunk_length)  # E: [[0, 0, 0], [0, 0, 0], [1, 1, 1]]
+    sample_nums = sample_nums.view(num_chunks_total * max_chunk_length)  # E: [0, 0, 0, 0, 0, 0, 1, 1, 1]
+
+    non_zero_sizes_mask = sizes != 0  # E: [[1, 1], [0, 0]]
+    batch_size_zeros = torch.zeros(batch_size, 1, dtype=sizes.dtype, device=sizes.device)
+    # E: torch.cat([batch_size_zeros, sizes[:, :-1]], 1).cumsum(1) =
+    # [[0, 2]
+    #  [0, 3]
+    intra_sample_shifts = torch.cat([batch_size_zeros, sizes[:, :-1]], 1).cumsum(1)[non_zero_sizes_mask]  # E: [0, 2, 0]
+
+    max_chunk_range = torch.arange(1, max_chunk_length + 1).view(1, max_chunk_length)
+    chunk_mask = max_chunk_range <= sizes[non_zero_sizes_mask].view(num_chunks_total, 1)
+    plain_indices = torch.arange(1, max_chunk_length + 1).view(1, max_chunk_length).repeat(num_chunks_total, 1)
+    print(f'plain_indices: {plain_indices}')
+    # E: plain_indices =
+    # [[1, 2, 3],
+    #  [1, 2, 3],
+    #  [1, 2, 3]]
+    indices = (plain_indices + intra_sample_shifts.view(num_chunks_total, 1)) * chunk_mask  # E: [[1, 2, 0], [3, 0, 0], [1, 2, 3]]
+    indices = indices.view(num_chunks_total * max_chunk_length)  # E: [1, 2, 0, 3, 0, 0, 1, 2, 3]
+
+    filler = expand_filler(fill_empty_val, inputs.shape).to(inputs.device).to(inputs.dtype)
+
+    inputs_with_filler_first = torch.cat([filler, inputs], dim=1)  # E: [[0, 1, 2, 3], [0, 4, 5, 6]]
+    # E: return [[1, 2, 0], [3, 0, 0], [4, 5, 6]]
+    return inputs_with_filler_first[sample_nums, indices].view(num_chunks_total, max_chunk_length, *inputs.shape[2:])
+
+
