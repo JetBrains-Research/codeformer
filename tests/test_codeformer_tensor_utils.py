2b965bdaca9766ee19808e8e8d0f3132ffbb4e79
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-07 19:59:48 +0100

    test: assemble tensor util

--- tests/test_codeformer_tensor_utils.py
+++ tests/test_codeformer_tensor_utils.py
@@ -1,5 +1,5 @@
 import torch
-from lm.utils import disassemble
+from lm.utils import disassemble, assemble
 
 
 def test_disassemble():
@@ -10,3 +10,23 @@ def test_disassemble():
                                     [3, 0, 0],
                                     [4, 5, 6]])
     assert torch.all(output == expected_output)
+
+
+def test_assemble():
+    inputs_asm = torch.tensor([[1, 2, 0],
+                               [3, 0, 0],
+                               [4, 5, 6]])
+    sizes = torch.tensor([[2, 1],
+                          [3, 0]])
+    fill_empty_val = 0
+    # sizes = torch.tensor([[2, 0], [1, 2]])
+
+    output = assemble(inputs_asm, sizes, fill_empty_val)
+
+    expected_output = torch.tensor([[[1., 2., 0.],
+                                     [3., 0., 0.]],
+                                    [[4., 5., 6.],
+                                     [0., 0., 0.]]])
+    assert torch.all(output == expected_output)
+
+
