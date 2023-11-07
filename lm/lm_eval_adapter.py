825e1a6e99d935a0654877d0452271d29d7dbde6
Misha Evtikhiev <mikhail.evtikhiev@jetbrains.com>
2023-11-07 14:37:16 +0200

    got stuck with batching tokens

--- lm/lm_eval_adapter.py
+++ lm/lm_eval_adapter.py
@@ -2,4 +2,18 @@ import torch
 from lm.utils import get_model_from_config, get_tokenizer_from_config
 from omegaconf import OmegaConf
 
-from lm_eval import evaluator, tasks
+from lm_eval.models.gpt2 import HFLM
+
+
+class EvalHarnessAdapter(HFLM):
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self._pretrained = "debertav2"
+        self.tokenizer = tokenizer
+        self.VOCAB_SIZE = tokenizer.vocab_size
+        self.EOT_TOKEN_ID = tokenizer.eos_token_id
+        self.batch_size_per_gpu = 1
+        self._max_length = self.tokenizer.model_max_length
+        self._device = "cpu"
+
+
--- lm/lm_eval_evaluator.py
+++ lm/lm_eval_evaluator.py
@@ -1,29 +1,33 @@
 import argparse
 import jsonlines, yaml
-
+from lm.utils import get_model_from_config, get_tokenizer_from_config
+from lm.lm_eval_adapter import *
 from lm_eval import evaluator, tasks
 
 
 def parse_args():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="/mnt/data/shared-data/codeformer/models/rand_init_codeformer.yaml", help="Config file location")
+    parser.add_argument("--config", type=str, default="/home/aspr/PycharmProjects/codeformer/artifacts/models/rand_init_codeformer.yaml", help="Config file location")
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = parse_args()
-    params = yaml.safe_load(open(args.config))
-
-    results = evaluator.evaluate(adaptor, tasks.get_task_dict(["scrolls_contractnli",
-                                                               "scrolls_govreport",
-                                                               "scrolls_narrativeqa",
-                                                               "scrolls_qasper",
-                                                               "scrolls_qmsum",
-                                                               "scrolls_quality",
-                                                               "scrolls_summscreenfd",]), False, 0, None)
+    #params = yaml.safe_load(open(args.config))
+    model = get_model_from_config(args.config)
+    tokenizer = get_tokenizer_from_config(args.config)
+    adapter = EvalHarnessAdapter(model, tokenizer)
+    results = evaluator.evaluate(adapter, tasks.get_task_dict(["scrolls_contractnli"]), False, 0, None)
+                                                               # "scrolls_govreport",
+                                                               # "scrolls_narrativeqa",
+                                                               # "scrolls_qasper",
+                                                               # "scrolls_qmsum",
+                                                               # "scrolls_quality",
+                                                               # "scrolls_summscreenfd",]), False, 0, None)
     with jsonlines.open('scrolls.jsonl', mode='w') as writer:
         for item in results:
             writer.write(item)
     print(results)
+
