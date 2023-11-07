6ac7c7712328d95fe37ae04b197f65f51353672d
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-07 20:29:04 +0100

    test: add assemble decoder input

--- tests/test_codeformer_tensor_utils.py
+++ tests/test_codeformer_tensor_utils.py
@@ -1,5 +1,5 @@
 import torch
-from lm.utils import disassemble, assemble
+from lm.utils import disassemble, assemble, assemble_decoder_inputs, expand_filler
 
 
 def test_disassemble():
@@ -30,3 +30,38 @@ def test_assemble():
     assert torch.all(output == expected_output)
 
 
+def test_assemble_decoder_inputs():
+    source = torch.tensor([[1, 2, 0], [3, 4, 5]]).unsqueeze(2)
+
+    # a)
+    # print(torch.tensor([, source[:, :-1].shape)
+    source_shifted = torch.cat([expand_filler(0, source.shape), source[:, :-1]], 1)
+
+    sizes = torch.tensor([[2, 1, 0],
+                          [1, 1, 1]],
+                         dtype=torch.long)
+
+    from_starting_points = torch.tensor([[0, 1, 0],
+                                         [0, 1, 2]],
+                                        dtype=torch.long)
+
+    to_starting_points = torch.tensor([[2, 1, 0],
+                                       [0, 0, 0]])
+
+    device = torch.device('cpu')
+    max_len = 5
+
+    output = assemble_decoder_inputs(source,
+                                     sizes,
+                                     from_starting_points,
+                                     to_starting_points,
+                                     max_len).squeeze(2)
+
+    expected_output = torch.tensor(
+        [[0, 0, 1, 2, 0],
+         [0, 2, 0, 0, 0],
+         [3, 0, 0, 0, 0],
+         [4, 0, 0, 0, 0],
+         [5, 0, 0, 0, 0]]
+    )
+    assert torch.all(output == expected_output)
