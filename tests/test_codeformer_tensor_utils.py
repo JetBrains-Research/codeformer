813a6875218316222f76790814ff1e5fdb64e3bb
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-08 14:37:27 +0100

    test: paddings to disassemble

--- tests/test_codeformer_tensor_utils.py
+++ tests/test_codeformer_tensor_utils.py
@@ -12,6 +12,40 @@ def test_disassemble():
     assert torch.all(output == expected_output)
 
 
+def test_disassemble_bos():
+    bos_id = 9
+    inputs = torch.arange(6).view(2, 3) + 1
+    sizes = torch.tensor([[2, 1], [3, 0]], dtype=torch.long)
+    output = disassemble(inputs, sizes, 0, bos_id)
+    expected_output = torch.tensor([[9, 1, 2, 0],
+                                    [9, 3, 0, 0],
+                                    [9, 4, 5, 6]])
+    assert torch.all(output == expected_output)
+
+
+def test_disassemble_eos():
+    eos_id = 9
+    inputs = torch.arange(6).view(2, 3) + 1
+    sizes = torch.tensor([[2, 1], [3, 0]], dtype=torch.long)
+    output = disassemble(inputs, sizes, 0, eos_value=eos_id)
+    expected_output = torch.tensor([[1, 2, 9, 0],
+                                    [3, 9, 0, 0],
+                                    [4, 5, 6, 9]])
+    assert torch.all(output == expected_output)
+
+
+def test_disassemble_bos_eos():
+    eos_id = 9
+    bos_id = 8
+    inputs = torch.arange(6).view(2, 3) + 1
+    sizes = torch.tensor([[2, 1], [3, 0]], dtype=torch.long)
+    output = disassemble(inputs, sizes, 0, bos_value=bos_id, eos_value=eos_id)
+    expected_output = torch.tensor([[8, 1, 2, 9, 0],
+                                    [8, 3, 9, 0, 0],
+                                    [8, 4, 5, 6, 9]])
+    assert torch.all(output == expected_output)
+
+
 def test_assemble():
     inputs_asm = torch.tensor([[1, 2, 0],
                                [3, 0, 0],
