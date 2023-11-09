f26853257647d544f9e543f46c0ec6dac19d6c89
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-09 11:19:01 +0100

    test: decoder assembling utils

--- tests/test_codeformer_tensor_utils.py
+++ tests/test_codeformer_tensor_utils.py
@@ -1,5 +1,7 @@
 import torch
-from lm.utils import disassemble, assemble, assemble_decoder_inputs, expand_filler
+from lm.utils import (disassemble, assemble, assemble_decoder_inputs,
+                      expand_filler, put_token_embeddings_at_specified_positions,
+                      prepare_token_ids_for_decoder)
 
 
 def test_disassemble():
@@ -99,3 +101,48 @@ def test_assemble_decoder_inputs():
          [5, 0, 0, 0, 0]]
     )
     assert torch.all(output == expected_output)
+
+
+def test_put_token_embeddings_at_specified_positions():
+    token_embs_by_chunk_flat = torch.tensor(
+        [[1, 2, 3, 0],
+         [1, 4, 5, 3]]
+    ).view(2, 4, 1)
+    expected_output = torch.tensor([[0, 0, 1, 2, 3, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 1, 4, 5, 3, 0, 0, 0]]).view(2, 10, 1)
+    start_positions = torch.tensor([2, 3])
+    max_len = 10
+    prev_and_curr_chunk_lens_plus_bos_eos = torch.tensor([3, 4])
+    output = put_token_embeddings_at_specified_positions(token_embs_by_chunk_flat,
+                                                         prev_and_curr_chunk_lens_plus_bos_eos,
+                                                         start_positions,
+                                                         max_len)
+    assert torch.all(output == expected_output)
+
+
+def test_prepare_token_ids_for_decoder():
+    token_ids = torch.tensor([
+        [1, 2, 3],
+        [4, 5, 0]
+    ])
+    sizes = torch.tensor([
+        [1, 2],
+        [1, 0]
+    ])
+    pad_id = 0
+    bos_id = 8
+    eos_id = 9
+
+    expected_output = torch.tensor([[8, 1, 9, 0, 0],
+                                    [8, 1, 2, 3, 9],
+                                    [8, 4, 9, 0, 0]])
+    expected_lens = torch.tensor([3, 5, 3])
+
+    output, lens = prepare_token_ids_for_decoder(token_ids,
+                                                 sizes,
+                                                 pad_id,
+                                                 bos_id,
+                                                 eos_id)
+
+    assert torch.all(output == expected_output)
+    assert torch.all(lens == expected_lens)
