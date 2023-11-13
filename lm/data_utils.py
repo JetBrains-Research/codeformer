05f80b0f784b3aa08f05737cea787b71b7457fff
Mikhail Arkhipov <arkhipovmu@gmail.com>
2023-11-13 10:32:27 +0100

    feat: add support of previous chunk inputs

--- lm/data_utils.py
+++ lm/data_utils.py
@@ -19,25 +19,33 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 @dataclass
 class TextTokens:
     token_ids: list[int] # can be code, not just text
-    split_sizes: list[int]
+    chunk_sizes: list[int]
 
     @property
     def num_splits(self) -> int:
-        return len(self.split_sizes)
+        return len(self.chunk_sizes)
 
     @property
-    def max_tokens_per_split(self) -> int:
-        return max(self.split_sizes)
+    def max_tokens_per_chunk(self) -> int:
+        return max(self.chunk_sizes)
 
 
 class BatchedTextTokens:
-    token_ids: Tensor
-    token_ids_chunk: Tensor
-    split_sizes_list: list[list[int]]
-    split_sizes_tensor: LongTensor
-    max_tokens_per_split: int
+    token_ids: LongTensor
+    token_ids_list: list[list[int]]
+    token_ids_bos_eos: LongTensor
+    token_ids_chunk: LongTensor
+    token_ids_chunk_bos_eos: LongTensor
+    token_ids_chunk_stacked: LongTensor
+    token_ids_chunk_stacked_bos_eos: LongTensor
+    chunk_sizes_list: list[list[int]]
+    chunk_sizes_tensor: LongTensor
+    max_tokens_per_chunk: int
     max_tokens_per_sample: int
-    max_splits: int
+    max_chunks_per_sample: int
+    max_tokens_per_chunk_bos_eos: int
+    max_tokens_per_sample_bos_eos: int
+    num_chunks_total: int
     token_ids_list: list[list[int]]
     pad_token_id: int
     bos_token_id: int
@@ -45,67 +53,156 @@ class BatchedTextTokens:
     att_mask: Tensor
     att_mask_chunk_tokens: Tensor
     att_mask_chunks: Tensor
+    batch_size: int
+    chunk_lens_stacked: LongTensor
+    decoder_inp_tok_ids: LongTensor
+    decoder_targ_tok_ids: LongTensor
 
     def __init__(self,
                  samples: List[TextTokens],
                  pad_token_id: int,
                  bos_token_id: int,
-                 eos_token_id: int) -> None:
-        # + 2 because of bos and eos tokens
-        self.max_tokens_per_split = max(s.max_tokens_per_split for s in samples) + 2
-        self.max_tokens_per_sample = max(len(s.token_ids) for s in samples) + 2
-        self.max_splits = max(s.num_splits for s in samples)
-        batch_size = len(samples)
-        self.token_ids_list = [s.token_ids for s in samples]
-        # self.split_sizes_list = [s.split_sizes for s in samples]
-        self.token_ids = pad_token_id * torch.ones(batch_size,
-                                                   self.max_tokens_per_sample,
-                                                   dtype=torch.long)
-        self.token_ids_chunk = pad_token_id * torch.ones(batch_size,
-                                                         self.max_splits,
-                                                         self.max_tokens_per_split,
-                                                         dtype=torch.long)
+                 eos_token_id: int,
+                 num_previous_chunks: int) -> None:
         self.pad_token_id = pad_token_id
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
+        self.num_previous_chunks = num_previous_chunks
+        self.max_tokens_per_chunk = max(s.max_tokens_per_chunk for s in samples)
+        self.max_tokens_per_chunk_bos_eos = self.max_tokens_per_chunk + 2
+        self.max_tokens_per_sample = max(len(s.token_ids) for s in samples)
+        self.max_tokens_per_sample_bos_eos = self.max_tokens_per_sample + 2
+        self.max_chunks_per_sample = max(s.num_splits for s in samples)
+        self.num_chunks_total = sum(s.num_splits for s in samples)
+        batch_size = len(samples)
+        self.batch_size = batch_size
+        self.token_ids_list = [s.token_ids for s in samples]
+        # self.chunk_sizes_list = [s.chunk_sizes for s in samples]
+        self.token_ids = torch.full((batch_size, self.max_tokens_per_sample),
+                                    pad_token_id,
+                                    dtype=torch.long)
+        self.token_ids_bos_eos = torch.full((batch_size, self.max_tokens_per_sample_bos_eos),
+                                            pad_token_id,
+                                            dtype=torch.long)
+        self.token_ids_chunk = torch.full([batch_size, self.max_chunks_per_sample, self.max_tokens_per_chunk],
+                                          pad_token_id, dtype=torch.long)
+        self.token_ids_chunk_bos_eos = torch.full([batch_size, self.max_chunks_per_sample, self.max_tokens_per_chunk_bos_eos],
+                                                  pad_token_id, dtype=torch.long)
+        self.token_ids_chunk_stacked = torch.full([self.num_chunks_total, self.max_tokens_per_chunk],
+                                                  pad_token_id, dtype=torch.long)
+        self.token_ids_chunk_stacked_bos_eos = torch.full([self.num_chunks_total, self.max_tokens_per_chunk + 2],
+                                                          pad_token_id, dtype=torch.long)
+        count = 0
+        self.chunk_lens_stacked = torch.zeros(self.num_chunks_total, dtype=torch.long)
         for sample_num, sample in enumerate(samples):
-            n_tokens_per_sample = len(sample.token_ids) + 2
-            sample_token_ids = [self.bos_token_id] + sample.token_ids + [self.eos_token_id]
+            n_tokens_per_sample = len(sample.token_ids)
+            n_tokens_per_sample_bos_eos = n_tokens_per_sample + 2
+            sample_token_ids = sample.token_ids
             self.token_ids[sample_num, :n_tokens_per_sample] = torch.tensor(sample_token_ids, dtype=torch.long)
+            sample_token_ids_bos_eos = [self.bos_token_id] + sample.token_ids + [self.eos_token_id]
+            self.token_ids_bos_eos[sample_num, :n_tokens_per_sample_bos_eos] = torch.tensor(sample_token_ids_bos_eos, dtype=torch.long)
             cursor = 0
-            for split_num, split_size in enumerate(sample.split_sizes):
-                chunk_tokens = [bos_token_id] + sample.token_ids[cursor: cursor + split_size] + [eos_token_id]
-                # + 2 because of bos and eos tokens
-                self.token_ids_chunk[sample_num, split_num, :split_size + 2] = torch.tensor(chunk_tokens, dtype=torch.long)
+            for split_num, split_size in enumerate(sample.chunk_sizes):
+                chunk_tokens = sample.token_ids[cursor: cursor + split_size]
+                self.token_ids_chunk_stacked[count, :len(chunk_tokens)] = torch.tensor(chunk_tokens, dtype=torch.long)
+                self.chunk_lens_stacked[count] = len(chunk_tokens)
+                self.token_ids_chunk[sample_num, split_num, :split_size] = torch.tensor(chunk_tokens, dtype=torch.long)
+                chunk_tokens_bos_eos = [bos_token_id] + sample.token_ids[cursor: cursor + split_size] + [eos_token_id]
+                self.token_ids_chunk_bos_eos[sample_num, split_num, :split_size + 2] = torch.tensor(chunk_tokens_bos_eos, dtype=torch.long)
+                self.token_ids_chunk_stacked_bos_eos[count, :len(chunk_tokens_bos_eos)] = torch.tensor(chunk_tokens_bos_eos, dtype=torch.long)
                 cursor += split_size
+                count += 1
         self.att_mask = (self.token_ids != torch.scalar_tensor(self.pad_token_id)).float()
+        self.att_mask_bos_eos = (self.token_ids_bos_eos != torch.scalar_tensor(self.pad_token_id)).float()
         self.att_mask_chunk_tokens = (self.token_ids_chunk != torch.scalar_tensor(self.pad_token_id)).float()
-        self.att_mask_chunks = torch.any(self.token_ids_chunk != self.pad_token_id, 2)
+        self.att_mask_chunk_tokens_stacked = (self.token_ids_chunk_stacked != torch.scalar_tensor(self.pad_token_id)).float()
+        self.att_mask_chunk_tokens_bos_eos = (self.token_ids_chunk_bos_eos != torch.scalar_tensor(self.pad_token_id)).float()
+        self.att_mask_chunk_tokens_stacked_bos_eos = (self.token_ids_chunk_stacked_bos_eos != torch.scalar_tensor(self.pad_token_id)).float()
+        self.att_mask_chunks = torch.any(self.token_ids_chunk != torch.scalar_tensor(self.pad_token_id), 2)
+        self.att_mask_chunks_bos_eos = torch.any(self.token_ids_chunk_bos_eos != torch.scalar_tensor(self.pad_token_id), 2)
 
-        self.split_sizes_list = []
+        self.chunk_sizes_list = []
+        self.chunk_sizes_list_bos_eos = []
         for s in samples:
-            self.split_sizes_list.append([split_size + 2 for split_size in s.split_sizes])
-        max_splits_per_sample = max(len(l) for l in self.split_sizes_list)
-        self.split_sizes_tensor = torch.zeros(batch_size, max_splits_per_sample, dtype=torch.long)
+            self.chunk_sizes_list.append([split_size for split_size in s.chunk_sizes])
+            self.chunk_sizes_list_bos_eos.append([split_size + 2 for split_size in s.chunk_sizes])
+        self.chunk_sizes_tensor = torch.zeros(batch_size, self.max_chunks_per_sample, dtype=torch.long)
+        self.chunk_sizes_tensor_bos_eos = torch.zeros(batch_size, self.max_chunks_per_sample, dtype=torch.long)
         for n in range(batch_size):
-            cur_split_sizes = torch.tensor(self.split_sizes_list[n], dtype=torch.long)
-            self.split_sizes_tensor[n, : len(cur_split_sizes)] = cur_split_sizes
+            cur_chunk_sizes = torch.tensor(self.chunk_sizes_list[n], dtype=torch.long)
+            self.chunk_sizes_tensor[n, : len(cur_chunk_sizes)] = cur_chunk_sizes
+            self.chunk_sizes_tensor_bos_eos[n, : len(cur_chunk_sizes)] = cur_chunk_sizes + 2
+
+        self.decoder_inp_tok_ids, self.decoder_targ_tok_ids = self._get_decoder_inputs(self.token_ids_list,
+                                                                                       self.chunk_sizes_list,
+                                                                                       self.pad_token_id,
+                                                                                       self.bos_token_id,
+                                                                                       self.eos_token_id,
+                                                                                       self.num_previous_chunks)
+        self.decoder_tok_mask = (self.decoder_inp_tok_ids != self.pad_token_id).float()
 
     def __len__(self) -> int:
         return len(self.token_ids_list)
 
-    def pin_memory(self) -> "BatchedTextTokens":
-        self.token_ids.pin_memory()
+    def to(self, device: torch.DeviceObjType) -> "BatchedTextTokens":
+        self.token_ids_chunk_bos_eos = self.token_ids_chunk_bos_eos.to(device)
+        self.att_mask_chunk_tokens = self.att_mask_chunk_tokens.to(device)
+        self.token_ids_chunk_stacked_bos_eos = self.token_ids_chunk_stacked_bos_eos.to(device)
+        self.chunk_sizes_tensor = self.chunk_sizes_tensor.to(device)
+        self.att_mask_chunks = self.att_mask_chunks.to(device)
+        self.decoder_inp_tok_ids = self.decoder_inp_tok_ids.to(device)
+        self.decoder_targ_tok_ids = self.decoder_targ_tok_ids.to(device)
         return self
 
-    def to(self, device: torch.DeviceObjType) -> "BatchedTextTokens":
-        self.token_ids = self.token_ids.to(device)
-        self.token_ids_chunk = self.token_ids_chunk.to(device)
-        self.att_mask = self.att_mask.to(device)
-        self.att_mask_chunk_tokens = self.att_mask_chunk_tokens.to(device)
-        self.att_mask_chunks = self.att_mask_chunks.to(device)
-        self.split_sizes_tensor = self.split_sizes_tensor.to(device)
-        return self
+    def _get_decoder_inputs(self,
+                            token_ids_list: list[list[int]],
+                            chunk_sizes_list: list[list[int]],
+                            pad_token_id: int,
+                            bos_token_id: int,
+                            eos_token_id: int,
+                            num_previous_chunks: int) -> tuple[LongTensor, LongTensor]:
+        decoder_inp_list = []
+        decoder_targ_list = []
+        for sample_num, chunk_sizes in enumerate(chunk_sizes_list):
+            num_chunks = len(chunk_sizes)
+            for chunk_num in range(num_chunks):
+                chunk_nums = list(range(chunk_num - num_previous_chunks, chunk_num + 1))
+                ids, num_toks_in_prev_chunks = self._get_multiple_chunk_token_ids(token_ids_list,
+                                                                                  chunk_sizes_list,
+                                                                                  sample_num,
+                                                                                  chunk_nums)
+                targets = [pad_token_id] * num_toks_in_prev_chunks + ids[num_toks_in_prev_chunks:] + [eos_token_id]
+                ids = [bos_token_id] + ids
+                assert len(targets) == len(ids)
+                decoder_inp_list.append(ids)
+                decoder_targ_list.append(targets)
+        num_chunks_total = len(decoder_inp_list)
+        max_len = max(len(ids) for ids in decoder_inp_list)
+        decoder_inp_token_ids = torch.full((num_chunks_total, max_len), pad_token_id, dtype=torch.long)
+        decoder_targ_token_ids = decoder_inp_token_ids.detach().clone()
+        for n_chunk, (inp_ids, targ_ids) in enumerate(zip(decoder_inp_list, decoder_targ_list)):
+            n_toks = len(inp_ids)
+            decoder_inp_token_ids[n_chunk, :n_toks] = torch.tensor(inp_ids, dtype=torch.long)
+            decoder_targ_token_ids[n_chunk, :n_toks] = torch.tensor(targ_ids, dtype=torch.long)
+        return decoder_inp_token_ids, decoder_targ_token_ids
+
+    def _get_multiple_chunk_token_ids(self,
+                                      token_ids_list: list[list[int]],
+                                      chunk_sizes_list: list[list[int]],
+                                      sample_num: int,
+                                      chunk_nums: int | list[int]) -> tuple[list[int], int]:
+        if isinstance(chunk_nums, int):
+            chunk_nums = [chunk_nums]
+        chunk_nums = set(chunk_nums)
+        sizes = chunk_sizes_list[sample_num]
+        start_end_points = torch.cumsum(torch.tensor([0] + sizes, dtype=torch.long), 0)
+        start_points = [sp for n, sp in enumerate(start_end_points[:-1]) if n in chunk_nums]
+        end_points = [ep for n, ep in enumerate(start_end_points[1:]) if n in chunk_nums]
+        start_point = start_points[0] if start_points else 0
+        end_point = end_points[-1] if end_points else 0
+        chunks_token_ids = token_ids_list[sample_num][start_point: end_point]
+        num_toks_in_prev_chunks = start_points[-1] - start_point
+        return chunks_token_ids, num_toks_in_prev_chunks
 
 
 class LMDatasetBase:
@@ -116,14 +213,16 @@ class LMDatasetBase:
             max_text_tokens: int,
             max_chunks_number: int,
             max_chunk_size: int,
+            num_previous_chunks: int,
             min_chunks: int = 1,
             min_tokens: int = 1
     ):
-        super(ThePileDataset).__init__()
+        super().__init__()
         self.split = split
         self.max_text_tokens = max_text_tokens
         self.max_chunks_number = max_chunks_number
         self.max_chunk_size = max_chunk_size
+        self.num_previous_chunks = num_previous_chunks
         self.tokenizer = tokenizer
         self._text_tree = MyTextTree()
         self.min_chunks = min_chunks
@@ -157,6 +256,7 @@ class ThePileDataset(LMDatasetBase, IterableDataset):
             max_text_tokens: int,
             max_chunks_number: int,
             max_chunk_size: int,
+            num_previous_chunks: int,
             min_chunks: int,
             min_tokens: int
     ):
@@ -165,6 +265,7 @@ class ThePileDataset(LMDatasetBase, IterableDataset):
                          max_text_tokens,
                          max_chunks_number,
                          max_chunk_size,
+                         num_previous_chunks,
                          min_chunks,
                          min_tokens)
 
@@ -193,6 +294,7 @@ class WikiTextDatasetBase(LMDatasetBase, Dataset, ABC):
             max_text_tokens: int,
             max_chunks_number: int,
             max_chunk_size: int,
+            num_previous_chunks: int,
             min_chunks: int,
             min_tokens: int
     ):
@@ -201,6 +303,7 @@ class WikiTextDatasetBase(LMDatasetBase, Dataset, ABC):
                          max_text_tokens,
                          max_chunks_number,
                          max_chunk_size,
+                         num_previous_chunks,
                          min_chunks,
                          min_tokens)
 
@@ -267,6 +370,7 @@ class AllDatasetsDataModule(LightningDataModule):
                  max_text_tokens: int,
                  max_chunks_number: int,
                  max_chunk_size: int,
+                 num_previous_chunks: int,
                  min_tokens: int = 1,
                  min_chunks: int = 1,
                  num_workers: int = 16,
@@ -279,6 +383,7 @@ class AllDatasetsDataModule(LightningDataModule):
         self.max_text_tokens = max_text_tokens
         self.max_chunks_number = max_chunks_number
         self.max_chunk_size = max_chunk_size
+        self.num_previous_chunks = num_previous_chunks
         self.min_tokens = min_tokens
         self.min_chunks = min_chunks
         self.num_workers = num_workers
@@ -288,7 +393,8 @@ class AllDatasetsDataModule(LightningDataModule):
         return BatchedTextTokens(batch,
                                  self.tokenizer.pad_token_id,
                                  self.tokenizer.bos_token_id,
-                                 self.tokenizer.eos_token_id)
+                                 self.tokenizer.eos_token_id,
+                                 self.num_previous_chunks)
 
     def _create_dataset(self, split: str):
         return self.name_to_dataset[self.dataset_name](split,
@@ -296,6 +402,7 @@ class AllDatasetsDataModule(LightningDataModule):
                                                        self.max_text_tokens,
                                                        self.max_chunks_number,
                                                        self.max_chunk_size,
+                                                       self.num_previous_chunks,
                                                        self.min_chunks,
                                                        self.min_tokens)
 
