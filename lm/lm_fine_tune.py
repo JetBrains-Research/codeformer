4128dd56bf7fa16ce6a901adbbc19ad6c7870558
Misha Evtikhiev <mikhail.evtikhiev@jetbrains.com>
2023-11-14 18:24:32 +0200

    inference and fine-tune for BART -- temp update

--- lm/lm_fine_tune.py
+++ lm/lm_fine_tune.py
@@ -9,6 +9,7 @@ import evaluate
 
 from transformers import AutoModelForSequenceClassification, AutoModelForCausalLM
 from transformers import AutoTokenizer
+from torch.nn import functional
 
 
 def compute_metrics(eval_pred):
@@ -21,23 +22,27 @@ def tokenize_function(tokenizer, examples, part, size):
     return (tokenizer(examples[part][0:size]["input"], padding="max_length", truncation=True, return_tensors='pt'),
             tokenizer(examples[part][0:size]["output"], padding="max_length", truncation=True, return_tensors='pt'))
 
-def bart_inference(dataset):
+
+def load_bart(dataset):
     raw_dataset = load_dataset(dataset)
     model = AutoModelForCausalLM.from_pretrained("facebook/bart-large", num_labels=3)
     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
     dataset_train = tokenize_function(tokenizer, raw_dataset, 'train', len(raw_dataset['train']))
     dataset_val = tokenize_function(tokenizer, raw_dataset, 'val', len(raw_dataset['val']))
     dataset_test = tokenize_function(tokenizer, raw_dataset, 'test', len(raw_dataset['test']))
+    return model, tokenizer, dataset_train, dataset_val, dataset_test
+
+
+def compute_loss(model, dataset, tokenizer, use_grad=False):
+    with torch.set_grad_enabled(use_grad):
+        model_logits = model(input_ids=dataset[0]['input_ids'], attention_mask=dataset[0]['attention_mask'], decoder_input_ids=dataset[1]['input_ids'], decoder_attention_mask=dataset[1]['attention_mask']).logits)
+        cross_entropy_tensor = functional.cross_entropy(model_logits[:,:-1].reshape(-1, model_logits.shape[-1]), dataset[1]['input_ids'][:,1:].reshape(-1),reduction="none", ignore_index=tokenizer.pad_token_id).reshape(model_logits.shape[0],model_logits.shape[1]-1)
+
 
 
 if __name__ == "__main__":
     metric = evaluate.load("accuracy")
-    raw_dataset = load_dataset("tau/scrolls", 'quality')
-    print(raw_dataset["train"][0])
-    model = AutoModelForSequenceClassification.from_pretrained("facebook/bart-large", num_labels=3)
-    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
-    dataset_train = tokenize_function(tokenizer, raw_dataset, 'train')
-    dataset_val = tokenize_function(tokenizer, raw_dataset, 'val')
+    model, tokenizer, dataset_train, dataset_val, dataset_test = load_bart("tau/scrolls", 'quality')
 
     training_args = TrainingArguments(output_dir="test_training", evaluation_strategy="steps")
     trainer = Trainer(
